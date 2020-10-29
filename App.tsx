5631a873c00f4ea925f2261fd7d5df13c5ca0e3e
Palloma Gualter <pallomagualter@gmail.com>
2020-10-29 11:35:15 -0300

    map

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <MapView 
+        style={styles.map} 
+        initialRegion={{
+          latitude: -5.0811659,
+          longitude: -42.7866835,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        />
     </View>
   );
 }
@@ -14,8 +21,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
