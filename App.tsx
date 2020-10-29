f2e166c92726c900357629db0d984837e894282f
Palloma Gualter <pallomagualter@gmail.com>
2020-10-29 16:31:29 -0300

    marker

--- App.tsx
+++ App.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
         <MapView 
-        style={styles.map} 
-        initialRegion={{
+          provider={PROVIDER_GOOGLE}
+          style={styles.map} 
+          initialRegion={{
           latitude: -5.0811659,
           longitude: -42.7866835,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }}
-        />
+        >
+          <Marker 
+            icon={mapMarker}
+            coordinate={{
+              latitude: -5.0811659,
+              longitude: -42.7866835,
+            }}
+          />
+        </MapView>
     </View>
   );
 }
